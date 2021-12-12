2949aa6e0491f4c3545bf9dca45695e8ed56d23e
MMuthubi254 <89958249+MMuthubi254@users.noreply.github.com>
2021-12-12 10:44:32 -0800

    commit

--- Express Crash Course/routes/api/members.js
+++ Express Crash Course/routes/api/members.js
@@ -27,6 +27,45 @@ router.post('/', (req, res)=>{
         email:req.body.email,
         status: 'active'
     }
+
+    if (!newMember.name || !newMember.email){
+        res.status(400).json({msg: 'please include a name and email'});
+    }
+
+    members.push(newMember);
+    res.json(members);
+
+});
+
+//update Member
+router.put('/:id', (req, res)=> {
+    const found = members.some(member => member.id ===parseInt(req.params.id));
+    if(found) {
+       const updMember = req.body;
+       members.forEach(member =>{
+        if(member.id === parseInt(req.params.id)){
+            member.name = updMember.name ? updMember.name : member.name;
+            member.email = updMember.email ? updMember.email : member.email;
+
+            res.json({msg: 'Member updated', member });
+            }
+
+       });
+    }else{
+        res.status(400).json({msg: `No member with the id of ${req.params.id}`});
+    }
+    
+});
+
+//Delete Memmber
+router.delete('/:id', (req, res)=> {
+    const found = members.some(member => member.id ===parseInt(req.params.id));
+    if(found) {
+        res.json({msg:'Member deleted', members:members.filter(member =>member.id !== parseInt(req.params.id))
+    });
+    } else {
+        res.status(400).json({msg: `No member with the id of ${req.params.id}`});
+    }
     
 });
 
